4fb9b21c31e408ec78748eaa523938f59234a4b7
ajrkhan1 <ajr.rgs@gmail.com>
2026-06-04 12:42:37 +0530

    dfg
    dfg

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -105,7 +105,7 @@ export default function Header() {
         <header class="header style-3 trasp">
                         <div class="header-inner">
                             <a href="/" reloadDocument class="site-logo">
-                                <img class="logo_header" alt="logo" width="200" height="52" src="/assets/images/logo/anvka-png.png"/>
+                                <img class="logo_header" alt="logo" width="200" height="52" src="/assets/images/logo.png"/>
                             </a>
                             <nav class="main-menu">
                                 <ul class="navigation ">
